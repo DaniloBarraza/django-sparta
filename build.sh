0d3045a76f4c30b269dc6a7f15f0000eaecefb5d
DaniloBarraza <danilobarraza08mail.com>
2025-01-09 12:25:37 -0300

    arreglo build

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
+
+# Navegar al directorio raíz del proyecto donde está manage.py
 cd DjangoWebProject2
-cd app
-# Convert static asset files
+
+# Recolectar archivos estáticos, indicando el directorio correcto para los archivos estáticos
 python manage.py collectstatic --no-input
